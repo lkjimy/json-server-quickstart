36aa5c3a340231541a787d411cc7cae082fc51ea
Lucas S <lucasjimy@hotmail.com>
2023-09-14 12:58:31 -0300

    Changes can be written to data files

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import dotenv from 'dotenv'
 import jsonServer from 'json-server'
 import routes from './routes.js'
@@ -14,12 +16,48 @@ const options = {
 }
 
 const PORT = process.env.JSON_SERVER_PORT || 3000
+const WRITE_TO_FILE = process.env.WRITE_TO_FILE || false
 const server = jsonServer.create()
 const router = jsonServer.router(routes.routes)
 const middlewares = jsonServer.defaults(options)
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
+
+// Writing changes to respective collections
+if (WRITE_TO_FILE) {
+  for (const route in routes.routes) {
+    server.use((req, res, next) => {
+      if (req._parsedUrl.pathname.includes(`/${route}`)) {
+        if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method)) {
+          function afterResponse() {
+            res.removeListener('finish', afterResponse)
+            res.removeListener('close', afterResponse)
+
+            // action after response
+            const collectionFile = path.resolve(`src/db/${route}/data.json`)
+            const data = JSON.stringify(router.db.getState()[route])
+
+            fs.writeFile(collectionFile, data, 'utf8', (error) => {
+              if (error) {
+                console.log(error)
+              }
+
+              console.log(
+                `data.json for collection '${route}' has been updated.`
+              )
+            })
+          }
+
+          res.on('finish', afterResponse)
+          res.on('close', afterResponse)
+        }
+      }
+
+      next()
+    })
+  }
+}
 
 for (const interceptor in routes.interceptors) {
   if (typeof routes?.interceptors[interceptor] === 'function') {
